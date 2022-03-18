78e3b8a4d7e5d16b19e41413cc1ce4fc5f346999
Vishal Bhosale <72347843+webdev-Vishal@users.noreply.github.com>
2022-03-18 17:53:00 +0530

    Initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { CarousalComponent } from './carousal/carousal.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillCardsComponent } from './skill-cards/skill-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HeroComponent,
+    CarousalComponent,
+    AboutMeComponent,
+    SkillCardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/skill-cards/skill-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-cards',
+  templateUrl: './skill-cards.component.html',
+  styleUrls: ['./skill-cards.component.scss']
+})
+export class SkillCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
