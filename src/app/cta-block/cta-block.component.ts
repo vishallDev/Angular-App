419b3a8d25b1816bdc2b8afb07dde422d490c050
Vishal Bhosale <72347843+webdev-Vishal@users.noreply.github.com>
2022-05-14 20:14:22 +0530

    bootstrap and skills card

--- src/app/cta-block/cta-block.component.ts
+++ src/app/cta-block/cta-block.component.ts
@@ -6,66 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cta-block.component.scss']
 })
 export class CtaBlockComponent implements OnInit {
-  ctaBlock: any = [
-    {
-      title: 'Front-end Developer',
-      info: 'I like to code things from scratch, and enjoy bringing ideas to life in the browser.',
-      img: 'assets/undraw_static_website_re_x70h.svg',
-      language_title: 'Languages I speak:',
-      tools_title: 'Tools I use:',
-      languages: [
-        { title: 'HTML' },
-        { title: 'PHP' },
-        { title: 'Wordpress' },
-        { title: 'JavaScript' },
-        { title: 'CSS' },
-        { title: 'Sass' },
-        { title: 'Pug' },
-        { title: 'SQL' },
-        { title: 'NOSQL' },
-        { title: 'Angular' },
-        { title: 'jQuery' }
-      ],
-      tools: [
-        { title: 'Bootstrap' },
-        { title: 'Wordpress' },
-        { title: 'Codepen' },
-        { title: 'Github' },
-        { title: 'Bitbucket' },
-        { title: 'Git' },
-        { title: 'Gulp' },
-        { title: 'Bower' },
-        { title: 'NPM' },
-        { title: 'Materialize' },
-        { title: 'Sketch' },
-        { title: 'Photoshop' }
-      ]
-    },
-    {
-      title: 'Wordpress/Shopify Developer',
-      info: 'I design and implement websites for companies using the WordPress creation tool. They are responsible for both front-end and back-end development,',
-      img: 'assets/undraw_control_panel_re_y3ar.svg',
-      language_title: 'Skills i am good at:',
-      tools_title: 'Extra knowledge of:',
-      languages: [
-        { title: 'Monitoring the performance of the live website' },
-        { title: 'Generating WordPress themes and plugins.' },
-        { title: 'Creating the website architecture.' },
-        { title: 'Designing and building the website front-end.' },
-        { title: 'Meeting with clients to discuss website design and function.' }
-      ],
-      tools: [
-        { title: 'Elementor' },
-        { title: 'Woocommerce' },
-        { title: 'Astra' },
-        { title: 'Media Query' },
-        { title: 'JScript' },
-        { title: 'WPBakery Page Builder' },
-        { title: 'good experience in customizations.' },
-        { title: 'Design focused with highly customizations' }
-      ]
-    }
-  ];
 
   constructor() { }
 
--- src/app/skill-cards/skill-cards.component.ts
+++ src/app/skill-cards/skill-cards.component.ts
@@ -6,6 +6,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skill-cards.component.scss']
 })
 export class SkillCardsComponent implements OnInit {
+  ctaBlock: any = [
+    {
+      title: 'Front End Developer',
+      info: 'I like to code things from scratch, and enjoy bringing ideas to life in the browser, I design and implement websites for companies using the WordPress creation tool. They are responsible for both front-end and back-end development.',
+      img: 'assets/card-block.svg',
+      language_title: 'Languages I speak:',
+      tools_title: 'Tools I use:',
+      extra_lang: 'Skills i am good at:',
+      extra_tools: 'Extra knowledge of:',
+      languages: [
+        { title: 'HTML' },
+        { title: 'PHP' },
+        { title: 'Wordpress' },
+        { title: 'JavaScript' },
+        { title: 'CSS' },
+        { title: 'Sass' },
+        { title: 'Pug' },
+        { title: 'SQL' },
+        { title: 'NOSQL' },
+        { title: 'Angular' },
+        { title: 'jQuery' }
+      ],
+      tools: [
+        { title: 'Bootstrap' },
+        { title: 'Wordpress' },
+        { title: 'Codepen' },
+        { title: 'Github' },
+        { title: 'Bitbucket' },
+        { title: 'Git' },
+        { title: 'Gulp' },
+        { title: 'Bower' },
+        { title: 'NPM' },
+        { title: 'Materialize' },
+        { title: 'Sketch' },
+        { title: 'Photoshop' }
+      ],
+      extra_languages: [
+        { title: 'Monitoring the performance of the live website' },
+        { title: 'Generating WordPress themes and plugins.' },
+        { title: 'Creating the website architecture.' },
+        { title: 'Designing and building the website front-end.' },
+        { title: 'Meeting with clients to discuss website design and function.' }
+      ],
+      e_tools: [
+        { title: 'Elementor' },
+        { title: 'Woocommerce' },
+        { title: 'Astra' },
+        { title: 'Media Query' },
+        { title: 'JScript' },
+        { title: 'WPBakery Page Builder' },
+        { title: 'good experience in customizations.' },
+        { title: 'Design focused with highly customizations' }
+      ]
+    }
+  ];
 
   constructor() { }
 
